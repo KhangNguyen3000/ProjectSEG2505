f0d83d9aa011fc8c2851ef121add98454e5ea4e4
Khang Nguyen <knguy082@uottawa.ca>
2018-11-07 19:36:19 -0500

    activity add

--- app/src/main/java/com/example/nguye/seg2505app/ServiceManagement.java
+++ app/src/main/java/com/example/nguye/seg2505app/ServiceManagement.java
@@ -1,7 +1,11 @@
 package com.example.nguye.seg2505app;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ServiceManagement extends AppCompatActivity {
 
@@ -9,6 +13,31 @@ public class ServiceManagement extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_management);
+    }
+    public void onClickDelete(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("Edit Service");
+        builder.setMessage("");//Metter le nom du service
+        builder.setPositiveButton("UPDATE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MyDBHandler dbHandler = new MyDBHandler(getApplicationContext());
+                String currentEmail = CurrentAccount.getCurrentAccount().getEmail();
+                dbHandler.deleteAccount(currentEmail);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // do nothing
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
