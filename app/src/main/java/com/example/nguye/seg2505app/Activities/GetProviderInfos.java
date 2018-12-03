18ae9e1f5cc7a7dac923dd8027a479524f731dc2
PaulSaladin <psala069@uottawa.ca>
2018-12-03 00:09:37 -0500

    Rating Database + Start of ProviderInfos layout

---
+++ app/src/main/java/com/example/nguye/seg2505app/Activities/GetProviderInfos.java
@@ -0,0 +1,94 @@
+package com.example.nguye.seg2505app.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.nguye.seg2505app.R;
+import com.example.nguye.seg2505app.Storables.Account;
+import com.example.nguye.seg2505app.Storables.OfferedService;
+import com.example.nguye.seg2505app.Storables.Rating;
+import com.example.nguye.seg2505app.Storables.Storable;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class GetProviderInfos extends AppCompatActivity {
+    private int id_provider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_provider_infos);
+        id_provider = (Integer) getIntent().getSerializableExtra("id_provider");
+        Account provider = new Account();
+        provider = provider.find(this, "ID", id_provider, false);
+
+
+        // Getting the screen elements
+        TextView p_Fname = findViewById(R.id.p_fName);
+        TextView p_lName = findViewById(R.id.p_lName);
+        TextView p_cName = findViewById(R.id.p_cName);
+        TextView p_licensed = findViewById(R.id.p_licensed);
+        TextView p_nStreet = findViewById(R.id.p_nStreet);
+        TextView p_street = findViewById(R.id.p_street);
+        TextView p_city = findViewById(R.id.p_city);
+        TextView p_postalC = findViewById(R.id.p_postalC);
+        TextView p_country = findViewById(R.id.p_country);
+        TextView p_nPhone = findViewById(R.id.p_nPhone);
+        TextView p_email = findViewById(R.id.p_email);
+        TextView p_schedule = findViewById(R.id.p_schedule);
+        TextView p_description = findViewById(R.id.p_description);
+        TextView p_services = findViewById(R.id.p_services);
+        RatingBar p_rating = findViewById(R.id.p_rating);
+
+        // Getting provider informations
+        ArrayList<OfferedService> servicesL = OfferedService.findAll(this, OfferedService.COL_PROVIDER, provider.getID(), false);
+        ArrayList<String> servicesName = ServiceManagementProvider.getNameFromServices(servicesL, getApplicationContext());
+        String services = "";
+        for(int i = 0; i < servicesName.size(); i++){
+            services += servicesName.get(i) + ", ";
+        }
+
+        double rating = 0;
+        ArrayList <Rating> ratings = Rating.findAll(this, Rating.COL_PROVIDER_ID, id_provider, false);
+        for(int i = 0; i < ratings.size(); i++){
+            rating += ratings.get(i).getRating();
+        }
+        rating = rating / ratings.size();
+
+        // Setting informations
+        p_Fname.setText(provider.getFirstName());
+        p_lName.setText(provider.getLastName());
+        p_cName.setText(provider.getCompanyName());
+        if(provider.getLicensed() == 1){
+            p_licensed.setText("Licensed");
+        }
+        else {
+            p_licensed.setText("");
+        }
+        p_nStreet.setText(provider.getStreetNumber());
+        p_street.setText(provider.getStreetName());
+        p_city.setText(provider.getCity());
+        p_postalC.setText(provider.getPostalCode());
+        p_country.setText(provider.getCountry());
+        p_nPhone.setText(provider.getPhoneNumber());
+        p_email.setText(provider.getEmail());
+        p_schedule.setText("See availabilities");
+        p_description.setText(provider.getDescription());
+        p_services.setText(services);
+        p_rating.setRating(rating);
+    }
+
+    public void onClickSchedule(){
+
+    }
+
+    public void onClickRating(){
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/nguye/seg2505app/Storables/Rating.java
@@ -0,0 +1,154 @@
+package com.example.nguye.seg2505app.Storables;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.nguye.seg2505app.MyDBHandler;
+
+import java.util.ArrayList;
+
+/**
+ * Service types established by the administrator.
+ * The providers will be able to offer one or more of those services
+ */
+public class Rating extends Storable {
+
+    // Structure of the table "ServiceTypes"
+    public static final String TABLE_NAME = "Ratings";
+    public static final String COL_ID = "ID";
+    public static final String COL_PROVIDER_ID = "ProviderID";
+    public static final String COL_CUSTOMER_ID = "ClientID";
+    public static final String COL_COMMENT = "Comment";
+    public static final String COL_RATING = "Rating";
+    public static final ArrayList<String[]> COLUMNS = new ArrayList<>();
+    static {
+        COLUMNS.add(new String[] {COL_ID, "INTEGER PRIMARY KEY AUTOINCREMENT"});
+        COLUMNS.add(new String[] {COL_PROVIDER_ID, "INTEGER"});
+        COLUMNS.add(new String[] {COL_CUSTOMER_ID, "INTEGER"});
+        COLUMNS.add(new String[] {COL_COMMENT, "TEXT"});
+        COLUMNS.add(new String[] {COL_RATING, "DOUBLE"});
+    }
+
+    // Attributes
+    private int p_ID;
+    private int c_ID;
+    private String description;
+    private double rating; // Max hourly rate of the service. The provider will not be able to set a higher hourly rate.
+
+    // Constructor
+    public Rating() {
+    }
+
+
+    // Constructor
+    public Rating(int pID, int cID, String description, double rating){
+        this.p_ID = pID;
+        this.c_ID = cID;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    // Constructor
+    public Rating(int ID, int pID, int cID, String description, double rating){
+        this.ID = ID;
+        this.p_ID = pID;
+        this.c_ID = cID;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    /**
+     * Put the ServiceType's attributes into a ContentValues object to use with database functions
+     * @return the ContentValues object
+     */
+    public ContentValues valuePutter() {
+        ContentValues values = new ContentValues();
+        values.put(COL_CUSTOMER_ID, getCID());
+        values.put(COL_PROVIDER_ID, getPID());
+        values.put(COL_COMMENT, getComment());
+        values.put(COL_RATING, getRating());
+        return values;
+    }
+
+    /**
+     * Search for the record that has the specified value in the specified field in the table "Services".
+     * @param fieldName Name of the column to look into
+     * @param value Value to search for
+     * @return The first record that matches.
+     */
+    public Rating find(Context context, String fieldName, Object value, boolean quotedValue) {
+        // Connect to the database
+        MyDBHandler dbHandler = new MyDBHandler(context);
+        SQLiteDatabase db = dbHandler.getReadableDatabase();
+
+        String quotes = "";
+        if (quotedValue) {
+            quotes = "\"";
+        }
+        String query = "SELECT * FROM " + this.getTableName()
+                + " WHERE " + fieldName + " = " + quotes + value + quotes;
+        System.out.println(query);
+
+        // Store the item's data into a Storable using a cursor
+        Cursor cursor = db.rawQuery(query, null);
+        Rating rating = null;
+        if(cursor.moveToFirst()) {
+            rating = new Rating();
+            rating.setID(cursor.getInt(getFieldIndex(COL_ID, COLUMNS)));
+            rating.setPID(cursor.getInt(getFieldIndex(COL_PROVIDER_ID, COLUMNS)));
+            rating.setCID(cursor.getInt(getFieldIndex(COL_CUSTOMER_ID, COLUMNS)));
+            rating.setComment(cursor.getString(getFieldIndex(COL_COMMENT, COLUMNS)));
+            rating.setRating(cursor.getDouble(getFieldIndex(COL_RATING, COLUMNS)));
+        }
+        db.close();
+        return rating;
+    }
+
+
+    // Getters and setters
+    public String getTableName() { return TABLE_NAME; }
+
+    public int getPID(){ return this.p_ID; }
+    public void setPID(int pID) { this.p_ID= pID; }
+
+    public int getCID(){ return this.c_ID; }
+    public void setCID(int cID) { this.c_ID= cID; }
+
+    public double getRating(){ return this.rating; }
+    public void setRating(double rating) { this.rating = rating; }
+
+    public String getComment(){return this.description;}
+    public void setComment(String comment){this.description = comment;}
+
+    public  static ArrayList<Rating> findAll(Context context, String fieldName, Object value, boolean quotedValue){
+        // Connect to the database
+        MyDBHandler dbHandler = new MyDBHandler(context);
+        SQLiteDatabase db = dbHandler.getReadableDatabase();
+
+        String quotes = "";
+        if (quotedValue) {
+            quotes = "\"";
+        }
+        String query = "SELECT * FROM " + Rating.TABLE_NAME
+                + " WHERE " + fieldName + " = " + quotes + value + quotes;
+        System.out.println(query);
+
+        Cursor cursor = db.rawQuery(query, null);
+        ArrayList<Rating> ratings = new ArrayList<>();
+        Rating rating = null;
+        while(cursor.moveToNext()){
+            rating = new Rating();
+            rating.setID(cursor.getInt(getFieldIndex(COL_ID, COLUMNS)));
+            rating.setPID(cursor.getInt(getFieldIndex(COL_PROVIDER_ID, COLUMNS)));
+            rating.setCID(cursor.getInt(getFieldIndex(COL_CUSTOMER_ID, COLUMNS)));
+            rating.setRating(cursor.getDouble(getFieldIndex(COL_RATING, COLUMNS)));
+            rating.setComment(cursor.getString((getFieldIndex(COL_COMMENT, COLUMNS))));
+            ratings.add(rating);
+        }
+        db.close();
+        return ratings;
+    }
+
+}
