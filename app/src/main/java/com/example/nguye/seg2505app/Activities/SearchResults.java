1d7ed36ccad1037f9ef64110ca68e5baa6934493
NicoDieryck <galardo33@gmail.com>
2018-12-02 19:33:24 -0500

    added add search result adapter activity

---
+++ app/src/main/java/com/example/nguye/seg2505app/Activities/SearchResults.java
@@ -0,0 +1,18 @@
+package com.example.nguye.seg2505app.Activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nguye.seg2505app.R;
+import com.example.nguye.seg2505app.ScheduleAdapter;
+
+public class SearchResults {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+
+}
