01dc98c0f70ef65b11cc9523fff6265b28f01e53
Khang Nguyen <knguy082@uottawa.ca>
2018-12-03 20:51:48 -0500

    update rating file

--- app/src/main/java/com/example/nguye/seg2505app/Activities/RateScreen.java
+++ app/src/main/java/com/example/nguye/seg2505app/Activities/RateScreen.java
@@ -3,8 +3,11 @@ package com.example.nguye.seg2505app.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 import com.example.nguye.seg2505app.R;
+import com.example.nguye.seg2505app.Storables.Account;
 import com.example.nguye.seg2505app.Storables.Rating;
 
 public class RateScreen extends AppCompatActivity {
@@ -15,9 +18,19 @@ public class RateScreen extends AppCompatActivity {
         setContentView(R.layout.activity_rate_screen);
     }
 
+    public void onClickRate(View view){
+        RatingBar rate = (RatingBar) view;
+    }
+
     public void onClickPost(View view) {
         int id_provider = (Integer) getIntent().getSerializableExtra("id_provider");
-        Rating rating = new Rating();
+        int id_customer = Account.getCurrentAccount().getID();
+        EditText commentaire = (EditText) findViewById(R.id.commentaire);
+        String description = commentaire.getText().toString();
+        float ratingNumber = ((RatingBar) findViewById(R.id.ratingBar2)).getRating();
+
+        Rating rating = new Rating(id_provider, id_customer, description, ratingNumber);
+        rating.add(this);
     }
 
 }
