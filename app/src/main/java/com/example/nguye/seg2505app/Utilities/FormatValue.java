e56d48b9b6920e154384a2c37dc8909b6a4cada3
Vincent <vmado051@uottawa.ca>
2018-12-05 19:19:22 -0500

    Changed the SearchForProviders activities to radio buttons

--- app/src/main/java/com/example/nguye/seg2505app/Utilities/FormatValue.java
+++ app/src/main/java/com/example/nguye/seg2505app/Utilities/FormatValue.java
@@ -88,6 +88,21 @@ public class FormatValue {
     }
 
     /**
+     * Convert a long representing a Date to the format yyyy-mm-dd
+     * @param dateLong
+     * @return
+     */
+    public static String longToDate(long dateLong) {
+        Date date = new Date(dateLong);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+
+        String dateString = cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.DAY_OF_MONTH);
+        dateString = dateYMD(dateString);
+        return dateString;
+    }
+
+    /**
      * Convert a time string to a long
      * @param timeString
      * @return
